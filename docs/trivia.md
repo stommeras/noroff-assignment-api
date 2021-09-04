19239ae8123eb4b1d573f0d3eb75ecb1a8cdb355
Dewald Els <dewald.els@noroff.no>
2021-09-04 07:00:36 +0200

    FEAT: (WIP) Authentication using simple API Key

---
+++ docs/trivia.md
@@ -0,0 +1,18 @@
+# Trivia API
+
+## Open Endpoints
+Open endpoints do not require authentication.
+
+### Trivia Users `GET /trivia`
+#### Sample Response
+```json
+[
+  {
+    "id": 1,
+    "username": "dewaldels",
+    "score": 0
+  }
+]
+```
+
+### Trivia Users `POST /trivia`
--- index.js
+++ index.js
@@ -5,7 +5,36 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 const { PORT = 3000 } = process.env
 
+const PROTECTED_HTTP_METHODS = ['POST', 'PATCH', 'DELETE', 'PUT']
+
 server.use(middlewares)
+
+server.use((request, response, next) => {
+
+    if (PROTECTED_HTTP_METHODS.includes(request.method)) {
+
+        const { authorization = '' } = request.headers
+        let token = null
+
+        if (authorization.includes('Bearer')) {
+            token = authorization.split(' ').pop()
+        } else {
+            token = authorization
+        }
+
+        if (!token) {
+            return response.status('401').json({ error: 'You are not allowed to access this resource' })
+        } else {
+            next()
+        }
+
+
+    }
+
+    next()
+
+})
+
 server.use(router)
 
 server.listen(PORT, () => {
